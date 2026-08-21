aecb2415cae5df300447ba14b8171f15135e58cd
Walter <wbrione@clemson.edu>
2026-08-21 12:22:18 -0400

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,35 @@
+'''
+    Stack implementation
+    Use a dictionary to pair closing brackets with their corresponding open brackets
+    Use for loop to iterate through s
+    Check if character in s is a key in dictionary pairs, if it is not append character to stack
+    If character is a key in pairs then pop top of stack and check that it is corresponding open bracket
+    If it is not then return False
+    After the loop if s is valid return True
+
+    edge cases:
+        "" = True
+        "[" = False
+        "[[[" = False
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {")" : "(", "}" : "{", "]" : "["}
+
+        if len(s) == 1:
+            return False
+
+        for c in s:
+            if c not in pairs:
+                stack.append(c)
+            else:
+                if stack:
+                    open_bracket = stack.pop()
+                else:
+                    open_bracket = ""
+                
+                if open_bracket != pairs[c]:
+                    return False
+        return len(stack) == 0
